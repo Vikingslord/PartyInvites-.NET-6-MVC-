710634afe33777675ad415d5acb4a26db60ed2b8
Vikingslord <ahsanulhaque1998@outlook.com>
2023-03-07 19:14:01 +0600

    Add project files.

---
+++ PartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PartyInvites.Models;
+using PartyInvites.Models.Repository;
+
+namespace PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            //int hour = DateTime.Now.Hour;
+            //ViewBag.Greeting = hour < 12 ? "Good Morning" : "Good Afternoon";
+            int hour = DateTime.Now.Hour;
+            ViewData["Greeting"] = hour < 12 ? "Good Morning" : "Good Afternoon";
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult RsvpForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult RsvpForm(GuestResponse guestResponse) 
+        {
+            if (ModelState.IsValid)
+            {
+                //To store response from user
+                Repository.AddResponse(guestResponse);
+                return View("Thanks", guestResponse);
+            }
+            else
+            {
+                return View();
+            }
+            
+        }
+
+        public IActionResult ListResponses()
+        {
+            return View(Repository.Responses.Where(r => r.WillAttend == true));
+        }
+    }
+}
